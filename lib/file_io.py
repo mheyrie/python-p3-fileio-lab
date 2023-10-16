97c342246d0f529e2028fe0d88bca67f58ef8d0a
Mary <omota4fidelity@gmail.com>
2023-10-16 20:10:13 +0100

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,17 +1,19 @@
-def write_file(file_name , file_content):
-    with open(f"{file_name}.txt, {file_content}, 'w'") as text_file:
-        return text_file.write
+def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", mode ='w', encoding= "utf-8") as text_file:
+        text_file.write(file_content)
+
+
 
 def append_file(file_name, append_content):
-    with open(f"{file_name}.txt, {append_content}, 'a'") as f:
-        return f.a
+    with open(f"{file_name}.txt", mode ='a', encoding= "utf-8") as text_file:
+        text_file.write(append_content)
         pass
 
-    pass
+    
 
 def read_file(file_name):
-    with open (f"{file_name}.txt", "r") as f:
-        return f.read
+    with open(f"{file_name}.txt", encoding= "utf-8") as text_file:
+        return text_file.read()
 
 
 # print(write_file(file_name="logfile", file_content="Log 1: 5 bananas added" ))
